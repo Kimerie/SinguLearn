b42806ead33521ba47e40604c982b2c12b24e5ac
kimerie <kimerie.green@gmail.com>
2016-05-12 19:34:50 -0700

    fix bug in server

--- Server/server.js
+++ Server/server.js
@@ -6,18 +6,18 @@ var methodOverride  = require('method-override')
 var mongoose        = require('mongoose')
 
 var port = process.env.PORT || 8000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res){
-  if (err) {
-    return console.error(err)
-  }
-  console.log('success')
-  res.send(res.body)
-})
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(methodOverride('X-HTTP-Method-Override'));
+// app.use(express.static(__dirname + '/public'));
+//
+// app.get('/', function(req, res){
+//   if (err) {
+//     return console.error(err)
+//   }
+//   console.log('success')
+//   res.send(res.body)
+// })
 
 // startup our app at http://localhost:8000
 app.listen(port);
