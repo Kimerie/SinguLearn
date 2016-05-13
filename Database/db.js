94f41597c4b754ddfdaa0c529f1fb2c70b177f93
kimerie <kimerie.green@gmail.com>
2016-05-13 12:24:44 -0700

    creating files for DB

---
+++ Database/db.js
@@ -0,0 +1,3 @@
+module.exports={
+  url: "mongodb://<dbuser>:<dbpassword>@ds023042.mlab.com:23042/heroku_thj53vw9"
+}
--- Server/server.js
+++ Server/server.js
@@ -4,12 +4,15 @@ var app             = express();
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override')
 var mongoose        = require('mongoose')
+var db              = require('../database/db.js')
+
+
 
 var port = process.env.PORT || 8000;
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(methodOverride('X-HTTP-Method-Override'));
-// app.use(express.static(__dirname + '/public'));
 //
 // app.get('/', function(req, res){
 //   if (err) {
@@ -18,6 +21,15 @@ var port = process.env.PORT || 8000;
 //   console.log('success')
 //   res.send(res.body)
 // })
+
+mongoose.connect(db.url)
+mongoose.connection.on('connected', function(err){
+  console.log('Default connection open to:' + db.url)
+});
+
+mongoose.connection.on('error', function(err){
+  console.log('connection error:' + err)
+});
 
 // startup our app at http://localhost:8000
 app.listen(port);
