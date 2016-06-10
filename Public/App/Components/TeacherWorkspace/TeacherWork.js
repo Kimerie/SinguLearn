af7d58c8dad93e3aa7c9a7d905db4ecc9172b18f
kimerie <kimerie.green@gmail.com>
2016-06-09 18:03:15 -0700

    updated code for demo purposes.

--- Public/App/Components/TeacherWorkspace/TeacherWork.js
+++ Public/App/Components/TeacherWorkspace/TeacherWork.js
@@ -5,29 +5,30 @@ teachWork.controller('TeachWorkCtrl',['$scope', 'Students', function ($scope, St
   
   $scope.studentInfo = [];
   $scope.names = [];
-  $scope.selected = " ";
+  $scope.selected = {};
   $scope.lessonNames = [];
 
   $scope.lessons = [
     {lessonType:"Analogies", lessonName: "Analogies: Life is a game. Play wisely.", id: 1},
     {lessonType:"Auxiliary Verbs",lessonName: "Auxiliary Verbs: Should've, Would've, Could've.", id: 2},
     {lessonType:"Superlatives",lessonName: "Superlatives: This or That (These or Those)? ", id: 3},
-    {lessonType:"Onomatopoeias",lessonName: "Onomatopoeias: Beep, beep. Who's got the key to the jeep? Vroom! ", id: 4},
+    {lessonType:"Onomatopoeias",lessonName: "Onomatopoeias: Beep, beep. Who's got the keys to the jeep? Vroom! ", id: 4},
 
   ];
 
-  $scope.$on('$typeahead.select', function(event, value, index, elem){
-        console.log(event); // event properties
-        console.log(value); // value of select
-        console.log(index); // index of selected value in dropdown
-        console.log(elem);  // properties of calling element ($id to get the id)
-});
 
 
+//   $scope.$on('$typeahead.select', function(event, value, index, elem){
+//         console.log(event); // event properties
+//         console.log(value); // value of select
+//         console.log(index); // index of selected value in dropdown
+//         console.log(elem);  // properties of calling element ($id to get the id)
+// });
 
-  $scope.changePlaylist = function(selected, playlistItems) {
-    console.log($scope.lessonNames)
-    var params = {"selected": $scope.selected, "playlistItems": $scope.lessonNames}
+
+  $scope.changePlaylist = function() {
+    console.log($scope.selected.name)
+    var params = {"selected": $scope.selected.name, "playlistItems": $scope.lessonNames}
     console.log(params)
     // Students.updatePlaylist(params).then(function(response) {
     //   console.log(response);
